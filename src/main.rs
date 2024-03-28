8e495820131b9526ac6fa7338839c15a8d2df124
Cristian Mejia <cristianmejia97@gmail.com>
2024-03-28 15:13:27 -0400

    Added basic print service

---
+++ src/main.rs
@@ -0,0 +1,107 @@
+use std::fmt::Debug;
+
+use printers;
+use headless_chrome;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Printer {
+    name: String,
+    is_default: bool,
+}
+
+fn get_printers_list() -> Vec<Printer> {
+    let mut parsed_printers= Vec::<Printer>::new();
+    let printers = printers::get_printers();
+
+    for printer in printers {
+        let printer_item = Printer {
+            name: printer.name,
+            is_default: printer.is_default,
+        };
+        parsed_printers.push(printer_item);
+    }
+
+    return parsed_printers;
+}
+
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Message {
+    message: String,
+}
+
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().json(Message {
+        message: "Hello, World!".to_owned(),
+    })
+}
+
+#[get("/printers")]
+async fn list_printers() -> impl Responder {
+    HttpResponse::Ok().json(get_printers_list())
+}
+
+#[derive(Deserialize)]
+struct PrintJobInput {
+    printer_name: String,
+    content: String,
+    format: String
+}
+
+#[post("/print")]
+async fn print(job: web::Json<PrintJobInput>) -> impl Responder {
+    let printer_name = &job.printer_name;
+    // let content = &job.content;
+
+    let printer = printers::get_printer_by_name(printer_name);
+    if printer.is_none() {
+        return Err(actix_web::error::ErrorNotFound("Printer not found"));
+    }
+
+    // if job.format == "html" {
+    let browser = headless_chrome::Browser::default().unwrap();
+    let tab = browser.new_tab().unwrap();
+    let data = format!("data:text/html,{}", &job.content.to_owned());
+    tab.navigate_to(&data).unwrap();
+    tab.wait_until_navigated().unwrap();
+    let content = tab.print_to_pdf(None).unwrap();
+    // }
+
+    let printer = printer.unwrap();
+    let result = printer.print(content.as_slice(), Some("Everything"));
+    if result.is_err() {
+        return Err(actix_web::error::ErrorInternalServerError("Failed to print"));
+    }
+
+    Ok(HttpResponse::Ok().json(Message {
+        message: "Printing".to_owned(),
+    }))
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .service(list_printers)
+            .service(print)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
