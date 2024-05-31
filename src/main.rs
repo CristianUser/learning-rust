87ee4786e2e1f33bb58db86a3b61d08a6aabd143
Cristian Mejia <cristianmejia97@gmail.com>
2024-05-31 14:21:16 -0400

    changed default port

--- src/main.rs
+++ src/main.rs
@@ -28,8 +28,8 @@ async fn main() -> std::io::Result<()> {
             .service(list_printers)
             .service(print)
     })
-    .bind(("127.0.0.1", 8080))?;
+    .bind(("127.0.0.1", 1829))?;
 
-    println!("Listening on port 8080");
+    println!("Listening on port 1829");
     server.run().await
 }
